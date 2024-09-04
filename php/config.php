ab86220556d7a1288a08110b7a9c9c688f30958e
Dani0215 <horvathdaniel.lkg@gmail.com>
2024-09-04 19:28:35 +0200

    Database

--- php/config.php
+++ php/config.php
@@ -5,11 +5,7 @@ $db = 'blog';
 $user = 'root';
 $pass = '';
 
-try{
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-    die("Could not connect to the database $db :" . $e->getMessage());
-}
+require_once 'Database.php';
+$database = new Database($host, $db, $user, $pass);
 
 ?>
---
+++ php/data/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+private $pdo;
+
+public function __construct($host, $db, $user, $pass) {
+
+    try{
+        $this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e){
+        die("Could not connect to the database $db :" . $e->getMessage());
+    }
+
+    public function getPdo() {
+        return $this->pdo;
+    }
+}
+
+
+?>
