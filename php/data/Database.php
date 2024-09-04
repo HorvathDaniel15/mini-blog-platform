0f43dd0eded18ab7908479e0f11b0591e29c57cd
Dani0215 <horvathdaniel.lkg@gmail.com>
2024-09-04 19:43:21 +0200

    database

--- php/data/Database.php
+++ php/data/Database.php
@@ -1,21 +1,24 @@
 <?php
 
-private $pdo;
+class Database {
 
-public function __construct($host, $db, $user, $pass) {
+    private $pdo;
 
-    try{
-        $this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch(PDOException $e){
-        die("Could not connect to the database $db :" . $e->getMessage());
+    public function __construct($host, $db, $user, $pass) {
+
+        try{
+            $this->pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e){
+            die("Could not connect to the database $db :" . $e->getMessage());
+        }
+
     }
 
     public function getPdo() {
         return $this->pdo;
     }
 }
-
 
 ?>
